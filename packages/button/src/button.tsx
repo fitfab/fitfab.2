da42ba2d7abe139dde2bb07a805c0557552e5e10
Miguel Julio <fitfab@yahoo.com>
2021-10-14 14:39:23 -0400

    refactor: add variant to the button and upadte stories

--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 
 import styled from "styled-components";
-
-const ButtonStyles = styled.button`
-  background-color: #c2005a;
+export interface ButtonProps {
+  /**
+   *  A string | React node to be rendered as the button's content
+   */
+  children?: React.ReactNode;
+  /**
+   * Determines the action of the button
+   */
+  variant?: "primary" | "secondary";
+}
+const ButtonStyles = styled.button<ButtonProps>`
+  background-color: ${(props) =>
+    props.variant === "primary" ? "#212121" : "#505050"};
   color: #fff;
   font-size: 1rem;
-  padding: 8px 16px;
+  padding: 9px 24px;
   border: none;
-  border-radius: 4px;
+  border-radius: 16px;
   cursor: pointer;
   transition: all 0.3s ease-in;
-  outline: 0px solid rgba(0, 0, 0, 0);
   &:hover {
-    background-color: #000;
-    outline-offset: 4px;
-    outline: 1px solid rgba(0, 0, 0, 0.3);
+    background-color: #ff4158;
+    outline-offset: 1px;
+    outline: 4px solid rgba(258, 65, 106, 0.3);
   }
 `;
-export interface ButtonProps {
-  children?: React.ReactNode;
-}
+
 export const Button = ({ children, ...rest }: ButtonProps) => (
   <ButtonStyles {...rest}>{children}</ButtonStyles>
 );
