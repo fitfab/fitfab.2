5b5d714ed5ffb02c78e2d5385bf40e6707de3b8e
Miguel Julio <fitfab@yahoo.com>
2021-10-20 15:14:28 -0400

    refactor: fitfab imports button from the dist folder

--- packages/fitfab/src/fitfab.ts
+++ packages/fitfab/src/fitfab.ts
@@ -2,4 +2,4 @@
  * This file exports all the `fitfab` component.
  * This is done to make it easier to import the component.
  */
-export * from "@fitfab/button/src/Button";
+export * from "@fitfab/button/dist/button";
