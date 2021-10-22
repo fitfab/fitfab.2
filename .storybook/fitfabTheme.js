15e7fdac632fecfa75eb16eabc8852b1311db85a
Miguel Julio <fitfab@yahoo.com>
2021-10-22 12:21:32 -0400

    docs: update theme and docs

---
+++ .storybook/fitfabTheme.js
@@ -0,0 +1,10 @@
+// .storybook/YourTheme.js
+
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "fitfab UI",
+  brandUrl: "http://fitfab.com",
+  brandImage: "https://place-hold.it/350x150",
+});
--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,9 @@ module.exports = {
     "../packages/**/*.stories.mdx",
     "../packages/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/theming",
+  ],
 };
